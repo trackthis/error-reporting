1c6241f322044bedcd3e5380807db2095f06d839
Robin Bron <robin@finwo.nl>
2018-08-27 11:58:21 +0200

    This should suffice

--- index.js
+++ index.js
@@ -1,138 +1,86 @@
-var slash = require('slashjs');
+var main = module.exports = function( options ) {
 
-/**
- * The module initializer
- *
- * @param options
- */
-var tter = module.exports = function( options ) {
-  if ( 'string' === typeof options ) { options = { scope: options }; }
-  options              = options || {};
-  options.scope        = options.scope || '';
-  options.scopeHash    = slash(options.scope);
-  options.report       = options.report || console.log;
-  options.reportArr    = options.reportArr || null;
-  options.level        = (options.level in tter.level || options.level) ? options.level : tter.level.INFO;
-  options.defaultLevel = (options.defaultLevel in tter.level || options.defaultLevel) ? options.defaultLevel : tter.level.INFO;
-  if(typeof options.level === 'string') {
-    options.level = (options.level.toUpperCase() in tter.level) ? parseInt(tter.level[options.level.toUpperCase()]) : tter.level.INFO;
-  }
-  if(typeof options.defaultLevel === 'string') {
-    options.defaultLevel = (options.defaultLevel.toUpperCase() in tter.level) ? parseInt(tter.level[options.defaultLevel.toUpperCase()]) : tter.level.INFO;
-  }
+  // Sanitize our inputs
+  if ('string' === typeof options) { options = {scope: options}; }
+  if ('object' !== options) options = {};
+  options = options || {};
 
-  /**
-   * The reporter function for the client
-   *
-   * @param level
-   * @param description
-   * @returns {*}
-   */
-  function reporter( level, description ) {
-    if ( 'undefined' === typeof description ) { 
-      description = level; 
-      level       = options.defaultLevel; 
-    }
-    if(typeof level === 'string') {
-      if (level.toUpperCase() in tter.level) {
-        level = parseInt(tter.level[level.toUpperCase()]);
-      } else {
-        level = tter.level.INFO;
-      }
-    }
-    if (!(level in tter.level)) {
-      level = tter.level.INFO;
-    }
-    if ( level > options.level ) {
-      return description;
-    }
-    var code = options.scopeHash + '.' + slash(description),
-        err  = { 
-          code        : code, 
-          level       : tter.level[level], 
-          description : description 
-        };
-    if (options.reportArr) {
-      options.reportArr.push(err);
-    }
-    options.report(err);
-    return description;
+  // Make the scope clear & track errors
+  options.scope     = options.scope || '';
+  options.reportArr = Array.isArray(options.reportArr) ? options.reportArr : false;
+
+  // Sanitize the reporters
+  options.reporters = (options.reporters||[]).reduce(function(reporters,reporter) {
+    if(!reporter) return reporters;
+    if('object'!==typeof reporter) return reporters;
+    if('function'!==typeof reporter.callback) return reporters;
+    if('string'===typeof reporter.level) reporter.level = main.level[reporter.level.toUpperCase()];
+    if('number'!==typeof reporter.level) return reporters;
+    reporters.push(reporter);
+    return reporters;
+  },[]);
+
+  // To clean ourselves from the stack
+  function cleanStack(stack) {
+    stack = stack.split("\r\n").join("\n");
+    stack = stack.split("\r").join("\n");
+    stack = stack.split("\n");
+    stack.shift(); // Error
+    stack.shift(); //   at index.js:<N>
+    return ['Error'].concat(stack).join("\n");
   }
 
-  /**
-   * Extend the current set of options & return a separate reporter for it
-   *
-   * @param extraOptions
-   * @returns {*}
-   */
+  // Reports a sanitized message to all listeners
+  function report( message ) {
+    var txtLevel = main.level[message.level],
+        msg      = Object.assign({},message,{level:txtLevel});
+    if(options.reportArr) options.reportArr.push(msg);
+    options.reporters.forEach(function(reporter) {
+      if ( reporter.level <= message.level ) return;
+      reporter.callback(msg);
+    });
+  }
+
+  // What we're returning to the caller
+  var reporter = function(message) {
+    return reporter.info({stack:cleanStack(new Error().stack),message});
+  };
+
+  // The common action for all levels
+  function commonReport( level, message ) {
+    var msg = { level: main.level[level], scope: options.scope, message: 'Fatal error', stack: false };
+    message = message || msg.message;
+    if('string'===typeof message) msg.message = message;
+    if('object'!==typeof message) message = {};
+    msg.stack   = message.stack   || cleanStack(new Error().stack);
+    msg.message = message.message || msg.message;
+    report(msg);
+  }
+
+  // Attach all levels
+  reporter.fatal   = commonReport.bind(undefined,'FATAL');
+  reporter.error   = commonReport.bind(undefined,'ERROR');
+  reporter.err     = commonReport.bind(undefined,'ERROR');
+  reporter.warning = commonReport.bind(undefined,'WARN');
+  reporter.warn    = commonReport.bind(undefined,'WARN');
+  reporter.info    = commonReport.bind(undefined,'INFO');
+  reporter.debug   = commonReport.bind(undefined,'DEBUG');
+  reporter.trace   = commonReport.bind(undefined,'TRACE');
+
+  // Allow forking a.k.a. extending the options
   reporter.fork = function( extraOptions ) {
-    if ( 'string' === typeof extraOptions ) {
-      extraOptions = { scope: extraOptions };
-    }
-    return tter(Object.assign({},options,extraOptions));
+    if ('string' === typeof extraOptions) extraOptions = {scope: extraOptions};
+    return main(Object.assign({},options,extraOptions));
   };
 
-  reporter.trace = function() {
-    var args = arguments;
-    return reporter.apply(reporter,[tter.level.TRACE].concat(Object.keys(args).map(function(k) {
-      return args[k];
-    })));
-  };
-
-  reporter.debug = function() {
-    var args = arguments;
-    return reporter.apply(reporter,[tter.level.DEBUG].concat(Object.keys(args).map(function(k) {
-      return args[k];
-    })));
-  };
-
-  reporter.info = function() {
-    var args = arguments;
-    return reporter.apply(reporter,[tter.level.INFO].concat(Object.keys(args).map(function(k) {
-      return args[k];
-    })));
-  };
-
-  reporter.warn = reporter.warning = function() {
-    var args = arguments;
-    return reporter.apply(reporter,[tter.level.WARN].concat(Object.keys(args).map(function(k) {
-      return args[k];
-    })));
-  };
-
-  reporter.err = reporter.error = function() {
-    var args = arguments;
-    return reporter.apply(reporter,[tter.level.ERROR].concat(Object.keys(args).map(function(k) {
-      return args[k];
-    })));
-  };
-
-  reporter.fatal = function() {
-    var args = arguments;
-    return reporter.apply(reporter,[tter.level.FATAL].concat(Object.keys(args).map(function(k) {
-      return args[k];
-    })));
-  };
-
+  // Filtering errors
   reporter.filterErrors = function(errLevel, reportArr) {
     reportArr = reportArr || options.reportArr;
-    if (!reportArr) {
-      return [];
-    }
-
-    if (typeof errLevel == 'string') {
-      if (errLevel.toUpperCase() in tter.level) {
-        errLevel = tter.level[errLevel.toUpperCase()];
-      } else {
-        errLevel = options.defaultLevel;
-      }
-    } else {
-      if (!(errLevel in tter.level)) {
-        errLevel = options.defaultLevel;
-      }
-    }
-    return reportArr.filter(function (err) {
-      return tter.level[err.level.toUpperCase()] <= errLevel;
+    if(!reportArr) return [];
+    if('string'===typeof errLevel) errLevel = main.level[errLevel.toUpperCase()];
+    if(!errLevel) return [];
+    return reportArr.filter(function(err) {
+      return errLevel > main.level[err.level.toUpperCase()];
     });
   };
 
@@ -140,7 +88,7 @@ var tter = module.exports = function( options ) {
 };
 
 // Register the logging levels
-tter.level = {
+main.level = {
   OFF   : 0, 0: 'OFF',
   FATAL : 1, 1: 'FATAL',
   ERROR : 2, 2: 'ERROR',
